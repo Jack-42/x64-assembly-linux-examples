c79a19911c531549e407a5a4cb016ac68c854d4f
Jack-42 <jakob-robert@hotmail.de>
2020-09-03 03:32:59 +0200

    helloworld: Use defines

--- 01-helloworld/helloworld.asm
+++ 01-helloworld/helloworld.asm
@@ -1,19 +1,23 @@
+%define SYS_WRITE 1
+%define SYS_EXIT 60
+%define STDOUT 1
+
 section .data
 	message db `Hello World!\n`
 	length equ $-message
 
 section .text
-	global _start
+	global _start			; define entry point
 
 _start:
 	; print to stdout (standard output)
-	mov rax, 1				; system call id: write
-	mov rdi, 1				; file descriptor: stdout
+	mov rax, SYS_WRITE		; system call id
+	mov rdi, STDOUT			; file descriptor
 	mov rsi, message		; address of buffer
 	mov rdx, length			; size of buffer
 	syscall
 
 	; exit program
-	mov rax, 60				; syscall id: exit
+	mov rax, SYS_EXIT		; system call id
 	mov rdi, 0				; exit code
 	syscall
