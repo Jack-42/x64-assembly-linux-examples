715881598f4400dd106353efb8570e4a7115505e
Jack-42 <jakob-robert@hotmail.de>
2020-09-03 02:53:05 +0200

    Add helloworld

---
+++ 01-helloworld/helloworld.asm
@@ -0,0 +1,19 @@
+section .data
+	message db "Hello World!", 10
+	length equ $-message
+
+section .text
+	global _start
+
+_start:
+	; print to stdout (standard output)
+	mov rax, 1			; syscall id: write
+	mov rdi, 1			; file descriptor: stdout
+	mov rsi, message	; address offer
+	mov rdx, length		; number of bytes
+	syscall
+
+	; exit program
+	mov rax, 60
+	mov rdi, 0
+	syscall
