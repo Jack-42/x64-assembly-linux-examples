d8475b32688021f53232176ba4867bc0bab6d281
Jack-42 <jakob-robert@hotmail.de>
2020-09-04 02:10:11 +0200

    helloworld-libc: Remove obsolete define

--- 01a-helloworld-libc/helloworld-libc.asm
+++ 01a-helloworld-libc/helloworld-libc.asm
@@ -2,8 +2,6 @@
 ; This program writes "Hello World!" to standard output using the C standard library. ;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-%define STDOUT 1
-
 global main                     ; make entry point visible to linker, this is called by the C library startup code
 extern puts                     ; declare C library function
 
